7b47a04266de886557d9ff583911434d3f36cead
oss.harits <haritsmedina@gmail.com>
2022-01-05 00:14:12 +0700

    navbar_debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import { Navbar, Welcome, Loader, Footer, Transaction } from "./components"
 
 const App = () => {
- 
-
   return (
     <div className="min-h-screen">
+      <h1>TEST</h1>
      <div className="gradient-bg-welcome">
-       <Navbar/>
-       <Welcome/>
+       <Navbar />
+       <Welcome />
      </div>
-     <Services/>
+     <Services />
      <Transaction/>
      <Footer/>
     </div>
