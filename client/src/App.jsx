091071ce9c4b80cbbad2ca21c45db5bb8e7ebc5e
oss.harits <haritsmedina@gmail.com>
2022-01-04 23:39:38 +0700

    initialize

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+ 
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
